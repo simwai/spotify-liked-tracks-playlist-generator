55c7fb774dac61f83e7b6380ac5e21a2e2d45490
simwai <16225108+simwai@users.noreply.github.com>
2024-04-27 19:19:06 +0200

    feat: :sparkles: Now, it appends to the playlist if the playlist already exists instead of creating a new one

--- index.py
+++ index.py
@@ -4,68 +4,86 @@ import config
 from flask import Flask, request
 from spotipy.oauth2 import SpotifyOAuth
 import threading
+from colorlog import ColoredFormatter
+from tqdm import tqdm
 
 app = Flask(__name__)
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+def setup_logger():
+    logger = logging.getLogger(__name__)
+    handler = logging.StreamHandler()
+    formatter = ColoredFormatter(
+        "%(log_color)s%(levelname)-8s%(reset)s %(blue)s%(message)s",
+        datefmt=None,
+        reset=True,
+        log_colors={
+            'DEBUG':    'cyan',
+            'INFO':     'green',
+            'WARNING':  'yellow',
+            'ERROR':    'red',
+            'CRITICAL': 'red,bg_white',
+        },
+        secondary_log_colors={},
+        style='%'
+    )
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    return logger
 
-# Spotify credentials
-client_id = config.CLIENT_ID
-client_secret = config.CLIENT_SECRET
-redirect_uri = config.REDIRECT_URI
-scope = 'user-library-read playlist-modify-public'
+logger = setup_logger()
 
-# Create a SpotifyOAuth instance to handle authentication
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
-                                               client_secret=client_secret,
-                                               redirect_uri=redirect_uri,
-                                               scope=scope))
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id=config.CLIENT_ID,
+    client_secret=config.CLIENT_SECRET,
+    redirect_uri=config.REDIRECT_URI,
+    scope='user-library-read playlist-modify-public'
+))
+
+def find_or_create_playlist(playlist_name, playlist_description):
+    playlists = sp.current_user_playlists()
+    for playlist in playlists['items']:
+        if playlist['name'] == playlist_name:
+            return playlist
+    return sp.user_playlist_create(user=sp.me()['id'], name=playlist_name, public=True, description=playlist_description)
+
+def add_tracks_to_playlist(playlist, tracks):
+    existing_tracks = sp.playlist_tracks(playlist['id'])
+    existing_track_uris = {track['track']['uri'] for track in existing_tracks['items']}
+    
+    new_tracks = [track['track']['uri'] for track in tracks if track['track']['uri'] not in existing_track_uris]
+    if new_tracks:
+        sp.user_playlist_add_tracks(user=sp.me()['id'], playlist_id=playlist['id'], tracks=new_tracks)
+        return len(new_tracks)
+    return 0
 
 def create_playlist_with_liked_tracks():
-    logger.info('Starting...')
     count_added_tracks = 0
     liked_tracks_limit = 50
-  
-    try:
-        # Get the user's liked tracks (max limit is 50)
-        liked_tracks = sp.current_user_saved_tracks(limit=liked_tracks_limit)
+    liked_tracks = sp.current_user_saved_tracks(limit=liked_tracks_limit)
+    playlist = find_or_create_playlist('LikedTracks', 'A playlist containing all my liked tracks on Spotify.')
 
-        # Create a new playlist
-        playlist_name = 'LikedTracks'
-        playlist_description = 'A playlist containing all my liked tracks on Spotify.'
-        playlist = sp.user_playlist_create(user=sp.me()['id'], name=playlist_name, public=True, description=playlist_description)
-
+    total_tracks = liked_tracks['total']
+    with tqdm(total=total_tracks, desc="Adding tracks to playlist", unit="track") as pbar:
         while liked_tracks['items']:
-            # Add liked tracks to the newly created playlist
-            track_uris = [track['track']['uri'] for track in liked_tracks['items']]
-            sp.user_playlist_add_tracks(user=sp.me()['id'], playlist_id=playlist['id'], tracks=track_uris)
-
-            count_added_tracks += len(track_uris)
+            added_tracks_count = add_tracks_to_playlist(playlist, liked_tracks['items'])
+            count_added_tracks += added_tracks_count
+            pbar.update(len(liked_tracks['items']))
 
             if liked_tracks['next']:
-                logger.info('Fetching more liked tracks...')
                 liked_tracks = sp.next(liked_tracks)
             else:
                 break
 
-        logger.info(f"Playlist '{playlist_name}' created with all {count_added_tracks} liked tracks.")
+    logger.info(f"Playlist 'LikedTracks' updated with {count_added_tracks} new liked tracks.")
 
-    except Exception as e:
-        logger.error(f"Error occurred: {e}")
-        
-# Create an endpoint to do OAuth flow
 @app.route('/callback')
 def callback():
-    global sp
-
     try:
         auth_code = request.args.get('code')
-        logger.info(f"Received authorization code: {auth_code}")
         sp.auth_manager.get_access_token(auth_code)
         logger.info("Authentication successful!")
         return "Authentication successful! You can close this window."
-
     except Exception as e:
         logger.error(f"Error occurred: {e}")
         return "An error occurred. Please check the logs for details.", 500
@@ -73,7 +91,5 @@ def callback():
 if __name__ == "__main__":
     thread = threading.Thread(target=app.run, kwargs={'port': 8888})
     thread.start()
-
     create_playlist_with_liked_tracks()
-
     thread.join()
